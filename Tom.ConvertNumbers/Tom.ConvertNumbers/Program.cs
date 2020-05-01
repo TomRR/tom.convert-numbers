a61da1f463f6d0dc512c59e1437f36c59afc887a
TomRR <resingtr@gmail.com>
2020-05-01 16:21:55 +0200

    init

---
+++ Tom.ConvertNumbers/Tom.ConvertNumbers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tom.ConvertNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
